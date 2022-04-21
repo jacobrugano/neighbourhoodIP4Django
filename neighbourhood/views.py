1b2dbbe6975faa24c002f2efcb599e8ec101667e
github_username <jacobrugano@gmail.com>
2022-04-21 15:46:21 +0300

    Created views

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def neighbourhood(request):
+    return render(request, 'neighbourhood/neighbourhood.html')
+
+
+def add_neighbourhood(request):
+    return render(request, 'neighbourhood/add_neighbourhood.html')
+
+
+def view_neighbourhood(request):
+    return render(request, 'neighbourhood/view_neighbourhood.html')
