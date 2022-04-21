eb4b07cc1d4b0f4e625c5cfdb92e814a0807ca07
github_username <jacobrugano@gmail.com>
2022-04-22 01:06:10 +0300

    Added models to database and admin page

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Neighborhood, Profile, Business, Post
+
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
